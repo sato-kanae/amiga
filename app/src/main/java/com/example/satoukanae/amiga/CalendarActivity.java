99aef95aeaa1d0ec090b11022bc25dcef786dc02
kpango <kpango@gmail.com>
2017-10-07 16:46:36 +0900

    calendar

---
+++ app/src/main/java/com/example/satoukanae/amiga/CalendarActivity.java
@@ -0,0 +1,57 @@
+package com.example.satoukanae.amiga;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * Shows off the most basic usage
+ */
+public class CalendarActivity extends AppCompatActivity implements OnDateSelectedListener, OnMonthChangedListener {
+
+    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
+
+
+    private MaterialCalendarView widget;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        widget =(MaterialCalendarView)findViewById(R.id.calendarView);
+        textView =(TextView) findViewById(  R.id.textView);
+        widget.setOnDateChangedListener(this);
+        widget.setOnMonthChangedListener(this);
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @Nullable CalendarDay date, boolean selected) {
+        textView.setText(getSelectedDatesString());
+    }
+
+    @Override
+    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+        //noinspection ConstantConditions
+        getSupportActionBar().setTitle(FORMATTER.format(date.getDate()));
+    }
+
+    private String getSelectedDatesString() {
+        CalendarDay date = widget.getSelectedDate();
+        if (date == null) {
+            return "No Selection";
+        }
+        return FORMATTER.format(date.getDate());
+    }
+}
