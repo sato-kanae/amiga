82a53d13a5f2d4028dfdcc8f9a564dc739d373c6
sato-kanae <32584493+sato-kanae@users.noreply.github.com>
2017-10-08 13:32:10 +0900

    Kanae2 (#7)
    * add recommendation
    
    * implement recommendation
    
    * revise User class
    
    * remove obsoluted

--- app/src/main/java/com/example/satoukanae/amiga/model/User.java
+++ app/src/main/java/com/example/satoukanae/amiga/model/User.java
@@ -10,15 +10,103 @@ public class User implements Serializable{
     private int timeIcon;
     private int faceIcon;
     private String name;
+    private String nickname;
+    private String faculty;
+    private String lang;
+    private String highschool;
+    private String hobby;
+    private String like;
+    private String club;
+    private String life;
 
-    public User(){
-
-    }
 
     public User(int timeIcon, int faceIcon, String name){
         this.timeIcon = timeIcon;
         this.faceIcon = faceIcon;
         this.name = name;
+    }
+
+    public User(int timeIcon, int FaceIcon,
+         String name, String nickname, String faculty, String lang,
+         String highschool, String hobby, String like,
+         String club,
+         String life){
+        this.timeIcon = timeIcon;
+        this.faceIcon = faceIcon;
+        this.name =   name;
+        this.nickname = nickname;
+        this.faculty = faculty;
+        this.lang = lang;
+        this.highschool = highschool;
+        this.hobby = hobby;
+        this.like = like;
+        this.club = club;
+        this.life = life;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getHighschool() {
+        return highschool;
+    }
+
+    public void setHighschool(String highschool) {
+        this.highschool = highschool;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getLike() {
+        return like;
+    }
+
+    public void setLike(String like) {
+        this.like = like;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public String getLife() {
+        return life;
+    }
+
+    public void setLife(String life) {
+        this.life = life;
+    }
+
+    public String getNickname() {
+
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 
     public int getTimeIcon() {
@@ -45,4 +133,27 @@ public class User implements Serializable{
         this.name = name;
     }
 
+    public float compare(User user){
+        float pfcl;
+        float plng;
+        float phs;
+
+        if(this.faculty.equals(user.faculty)){
+            pfcl = 1;
+        }else{
+            pfcl = 0;
+        }
+        if(this.lang.equals(user.lang)){
+            plng = 1;
+        }else{
+            plng = 0;
+        }
+        if(this.highschool.equals(user.highschool)){
+            phs = 1;
+        }else{
+            phs = 0;
+        }
+
+        return pfcl + plng + phs;
+    }
 }
